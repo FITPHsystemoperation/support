0d1cf97829aa03e1de53407f68140680b5ad716f
Arnel Forbes <forbesarnel09@gmail.com>
2018-11-14 15:23:50 +0900

    Updates

---
+++ precache-manifest.5a14f28a5317fb7c33cd6e10ee295acf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "04ecbaf0cc0e0f362ccfee40e0d0fd30",
+    "url": "./static/media/cad3.04ecbaf0.jpg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "d69eff2f77e5084c7494",
+    "url": "./static/js/main.d69eff2f.chunk.js"
+  },
+  {
+    "revision": "c2ea6c283125f00601cc",
+    "url": "./static/js/1.c2ea6c28.chunk.js"
+  },
+  {
+    "revision": "d69eff2f77e5084c7494",
+    "url": "./static/css/main.f81f5ecb.chunk.css"
+  },
+  {
+    "revision": "e2bc0816bfe3b6aece73559d805fcf70",
+    "url": "./index.html"
+  }
+];
