c8fd4fc3c80d04d10a7bfaa48762c9d067493890
Arnel Forbes <forbesarnel09@gmail.com>
2018-11-19 10:44:50 +0800

    main rendered

--- src/index.js
+++ src/index.js
@@ -32,6 +32,7 @@ class Section extends React.Component {
 			<section id={this.props.value}>
 				<div className="title">{this.props.value}</div>
 				<div className="content">{this.renderLink(this.props.links)}</div>
+				{this.props.others}
 			</section>
 		);
 	}
@@ -55,112 +56,56 @@ const sections = [
 		title: "Monitoring",
 		links: {
 			"Plan Monitoring (August 2018)": "https://docs.google.com/spreadsheets/d/1f4CM19eSHN_OdDZY39z4HAi1b_Ib4sfIP3aVderSdTM/edit#gid=925288811",
-			"":"",
+			"Mistake Monitoring (August 2018)":"https://docs.google.com/spreadsheets/d/10ZE2AkGJCynve6tog7xT8NkGqFI-ZF36TbaA8EO89yw/edit#gid=0",
+			"Plan Monitoring v2.0":"https://docs.google.com/spreadsheets/d/1EeV-O9JyWU_l0LW5g0onSShgqolUA6ecxbkS7GvDcyw/edit#gid=853151862",
+			"Plan Monitoring v2.1":"https://docs.google.com/spreadsheets/d/1quD2imwF8rqBQUzq6-m8lrqKBnPz1WRk5uzez7mwZFw/edit#gid=472071419",
+			"Plan Monitoring v2.2 (November)":"https://docs.google.com/spreadsheets/d/1cdXoLbqUIzCjVEjcnhuEuX5EHR5ZwH6rJDFXvTGOKU4/edit#gid=472071419",
 		},
 		others: ""
+	},
+	{
+		title: "Manual",
+		links: {
+			"Check List":"../misc/pdf/checklist10.01.2018.pdf",
+			"Checking Manual":"../misc/pdf/manual_for_checking10.01.2018.pdf",
+			"Basic Detailing":"../misc/pdf/dp.pdf",
+			"Basic Electrical Plan":"../misc/pdf/ep.pdf",
+			"DP & EP (Updated)":"../misc/pdf/dpep10.02.2018.pdf",
+			"Storage Plan (Actie)":"../misc/pdf/sp.pdf",
+		},
+		others: ""
+	},
+	{
+		title: "Report",
+		links: {
+			"Monthly Comparison":"https://docs.google.com/spreadsheets/d/1S0FXjVacFKHxqwcVHWoMKkSI9NckSquD0DjNQzzNNMg/edit#gid=1015423851",
+			"Productivity & Quality Report":"https://docs.google.com/spreadsheets/d/1ZkU9-HDHKubtWJlStzilFwa-C-RDmr4-1sgm0CpCbnk/edit#gid=1537691498",
+		},
+		others: ""
+	},
+		{
+		title: "Kakunin Monitoring",
+		links: {
+			"Mistake Encoding":"https://goo.gl/forms/X8lNbWjhq7LFVmnt1",
+			"Mistake Lists":"https://docs.google.com/spreadsheets/d/1Gd-GJ7ZHVfvRf3xZiz5K9UDrCUad3Zv_oeb9pKNIDgA/edit?usp=sharing",
+		},
+		others: ""
+	},
+		{
+		title: "Additional Links",
+		links: {
+			"Spread Sheet (Project)":"https://docs.google.com/spreadsheets/d/1hD1sYnf_btSktRM8SiYJStvzM5hsmdZHoROt4tSWCOU/edit?ts=584e3927#gid=464436514",
+			"Spread Sheet (JW Convertion)":"https://docs.google.com/spreadsheets/d/1jKAao-2QcKtGUz5ZBXxoAusP0Ulcsj9hn1R91EBcPtk/edit#gid=1563213429",
+			"Gmail":"https://mail.google.com/mail/u/1/#inbox",
+			"Google Translate":"https://translate.google.com/",
+			"Data Bank":"https://www.databank-solution.net/",
+			"Fire Storage":"http://firestorage.jp/",
+		},
+		others: <div className="note">*Your suggested links and shortcuts will be here...</div>
 	}
 ]
 
 ReactDOM.render(<Main content={sections} />, document.getElementById('root'));
-
-// const content = (
-// 	<React.Fragment>
-// 		<div className="layer2">
-// 			<div className="background"></div>
-// 		</div>
-// 		<div className="layer1">
-// 			<div className="filter"></div>
-// 		</div>
-// 		<div className="layer0">
-// 			<main>
-// 				<div id="home" className="blank"></div>
-// 				<section id="monitoring">
-// 					<div className="title">Monitoring</div>
-// 					<div className="content">
-// 						<a href="" target="_blank" rel="noopener noreferrer"></a>
-// 						<a href="" target="_blank" rel="noopener noreferrer"></a>
-// 						<a href="https://docs.google.com/spreadsheets/d/1quD2imwF8rqBQUzq6-m8lrqKBnPz1WRk5uzez7mwZFw/edit#gid=472071419" target="_blank" rel="noopener noreferrer">Plan Monitoring v2.1 (October)</a>
-// 						<a href="https://docs.google.com/spreadsheets/d/1cdXoLbqUIzCjVEjcnhuEuX5EHR5ZwH6rJDFXvTGOKU4/edit#gid=472071419" target="_blank" rel="noopener noreferrer">Plan Monitoring v2.2 (November)</a>
-// 					</div>
-// 				</section>
-// 				<section id="manual">
-// 					<div className="title">Manual</div>
-// 					<div className="content">
-// 						<a href="../misc/pdf/checklist10.01.2018.pdf" target="_blank" rel="noopener noreferrer">Check List</a>
-// 						<a href="../misc/pdf/manual_for_checking10.01.2018.pdf" target="_blank" rel="noopener noreferrer">Checking Manual</a>
-// 						<a href="../misc/pdf/dp.pdf" target="_blank" rel="noopener noreferrer">Basic Detailing</a>
-// 						<a href="../misc/pdf/ep.pdf" target="_blank" rel="noopener noreferrer">Basic Electrical Plan</a>
-// 						<a href="../misc/pdf/dpep10.02.2018.pdf" target="_blank" rel="noopener noreferrer">DP & EP (Updated)</a>
-// 						<a href="../misc/pdf/sp.pdf" target="_blank" rel="noopener noreferrer">Storage Plan (Actie)</a>
-// 					</div>
-// 				</section>
-// 				<section id="report">
-// 					<div className="title">Report</div>
-// 					<div className="content">
-// 						<a href="https://docs.google.com/spreadsheets/d/1S0FXjVacFKHxqwcVHWoMKkSI9NckSquD0DjNQzzNNMg/edit#gid=1015423851" target="_blank" rel="noopener noreferrer">Monthly Comparison</a>
-// 						<a href="https://docs.google.com/spreadsheets/d/1ZkU9-HDHKubtWJlStzilFwa-C-RDmr4-1sgm0CpCbnk/edit#gid=1537691498" target="_blank" rel="noopener noreferrer">Productivity & Quality Report</a>
-// 					</div>
-// 				</section>
-// 				<section id="kakunin">
-// 					<div className="title">Kakunin Mistake Monitoring</div>
-// 					<div className="content">
-// 						<a href="https://goo.gl/forms/X8lNbWjhq7LFVmnt1" target="_blank" rel="noopener noreferrer">Mistake Encoding</a>
-// 						<a href="https://docs.google.com/spreadsheets/d/1Gd-GJ7ZHVfvRf3xZiz5K9UDrCUad3Zv_oeb9pKNIDgA/edit?usp=sharing" target="_blank" rel="noopener noreferrer">Mistake Lists</a>
-// 					</div>
-// 				</section>
-// 				<section id="additional">
-// 					<div className="title">Additional Links</div>
-// 					<div className="content">
-// 						<a href="https://docs.google.com/spreadsheets/d/1hD1sYnf_btSktRM8SiYJStvzM5hsmdZHoROt4tSWCOU/edit?ts=584e3927#gid=464436514" target="_blank" rel="noopener noreferrer">Spread Sheet (Project)</a>
-// 						<a href="https://docs.google.com/spreadsheets/d/1jKAao-2QcKtGUz5ZBXxoAusP0Ulcsj9hn1R91EBcPtk/edit#gid=1563213429" target="_blank" rel="noopener noreferrer">Spread Sheet (JW Convertion)</a>
-// 						<a href="https://mail.google.com/mail/u/1/#inbox" target="_blank" rel="noopener noreferrer">Gmail</a>
-// 						<a href="https://translate.google.com/" target="_blank" rel="noopener noreferrer">Google Translate</a>
-// 						<a href="https://www.databank-solution.net/" target="_blank" rel="noopener noreferrer">Data Bank</a>
-// 						<a href="http://firestorage.jp/" target="_blank" rel="noopener noreferrer">Fire Storage</a>
-// 					</div>
-// 					<div className="note">*Your suggested links and shortcuts will be here...</div>
-// 				</section>
-// 				<div className="blank"></div>
-// 			</main>
-// 			<div className="header">
-// 				<div className="title">Training Department</div>
-// 				<div className="sub-title">Projects and Links</div>
-// 				<div className="button">
-// 					<input type="button" id="start" data-target="monitoring" value="Get Started" />
-// 				</div>
-// 			</div>
-// 			<nav className="nav">
-// 				<div data-target="home">Home</div>
-// 				<div data-target="monitoring">Monitoring</div>
-// 				<div data-target="manual">Manual</div>
-// 				<div data-target="report">Report</div>
-// 				<div data-target="kakunin">Kakunin</div>
-// 				<div data-target="additional">Additional</div>
-// 			</nav>
-// 			<div className="post-it">
-// 				<div className="tape tape1"></div>
-// 				<div className="tape tape2"></div>
-// 				<p></p>
-// 				<p>Updates as of : <strong>Nov 13, 2018</strong></p>
-// 				<p></p>
-// 				<p>Additional Link:</p>
-// 				<p></p>
-// 				<p>Kakunin Mistake Monitoring</p>
-// 				<p></p>
-// 				<p>Click <span className="note-link" data-target="kakunin">here</span> to view.</p>
-// 				<p></p>
-// 				<p></p>
-// 				<p></p>
-// 				<p></p>
-// 				<p></p>
-// 				<p></p>
-// 				<p></p>
-// 			</div>
-// 		</div>
-// 	</React.Fragment>
-// )
-
-// ReactDOM.render(content, document.getElementById('root'));
 
 // const background = document.querySelector('.background');
 // const nav = document.querySelector('.nav')
